df48a8b777705394541a1c588d9432eab25aa94f
Rafael Moraes <jrafaelm@gmail.com>
2016-10-24 19:32:55 -0300

    adding googlemaps and creating list

--- WeatherForecast/WeatherForecast/CitiesTableViewController.swift
+++ WeatherForecast/WeatherForecast/CitiesTableViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class CitiesTableViewController: UITableViewController {
 
+    var citiesDictionaries : [String: Any]?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,13 +26,15 @@ class CitiesTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        if citiesDictionaries != nil {
+            return citiesDictionaries!.count
+        }else{
+            return 0
+        }
     }
 
     /*
--- WeatherForecast/WeatherForecast/MapViewController.swift
+++ WeatherForecast/WeatherForecast/MapViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController, GMSMapViewDelegate {
 
+    @IBOutlet weak var mapView : GMSMapView?
+    var selectedMarker : GMSMarker?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView?.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "pushList" {
+            if selectedMarker != nil {
+                
+            }else{
+                let alertController = UIAlertController(title: "Hey!", message: "Tap on the map to see the forecast of the region", preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+                
+            }
+        }
+    }
+    
+    //MARK: GMSMapViewDelegate
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        
+        if self.selectedMarker == nil {
+            self.selectedMarker = GMSMarker.init();
+        }
+        self.selectedMarker!.position = coordinate;
+        self.selectedMarker!.title = "Search";
+        self.selectedMarker!.snippet = "Forecast for this region";
+        self.selectedMarker!.map = self.mapView;
+    }
 }
 
